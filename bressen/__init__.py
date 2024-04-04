b9868fa9da00e28044f7e4ad6b417220f61a25dc
DanielTollenaar <daniel@d2hydro.nl>
2024-04-04 16:22:28 +0200

    BasisGegevens class

--- bressen/__init__.py
+++ bressen/__init__.py
@@ -1,7 +1,8 @@
 from bressen.io import read_directory, read_files
+from bressen.basisgegevens import BasisGegevens
 import warnings
 
-__version__ = "2024.3.0"
-__all__ = ["read_directory", "read_files"]
+__version__ = "2024.4.0"
+__all__ = ["BasisGegevens", "read_directory", "read_files"]
 
 warnings.filterwarnings("ignore", module="pyogrio")
